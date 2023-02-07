85aeaea9a3576f3c3097c638aa9a79df3f8fc962
omar <omar.daoudi.etu@gmail.com>
2023-02-07 11:53:29 +0100

    nothing

--- decodewebpage.py
+++ decodewebpage.py
@@ -7,6 +7,6 @@ r_html=r.text
 soup=BeautifulSoup(r_html,'lxml')
 i=1
 for link in soup.find_all('h3') :
-     if link.get_text() != "Advertisement":       
-        print(link.get_text())
-        i=i+1   
+   if link.get_text() != "Advertisement":   
+      print(link.get_text())
+      i=i+1   
